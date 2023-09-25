94a7bcfaa351dee54717c4f277b5509c9368413d
Hardeep Singh <hs0774@gmail.com>
2023-09-25 13:44:50 -0400

    Cart now displays items in cart,though not in order of purchase, and if cart is empty,total is next

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -21,26 +21,48 @@ interface Props {
 
 const Cart: React.FC<Props> = ({data,setData}) => {
     const [cart,setCart] = React.useState<Item[]>(data);
-    React.useEffect(() => {
-        if(data && data.length>0){
-            setCart(data);
-        }
-        console.log(data)
-    }, []);
+
+    // React.useEffect(() => {
+    //     if(data && data.length===0){
+    //         return <p>Cart is empty!</p>
+    //     }
+    //     console.log(data)
+    // }, []);
 
     return (
         <div className="Cart">
             <div className="CartLeft">
-                {cart[0].inCart ?
-                <p>{cart[0].quantity}</p>:<p>Cart is empty shop now!</p>}
-            {/* make a div with buy items if cart is empty  
-            here is list of items with pic ,editable quantity, total,x button  */}
+                {cart.some(item => item.inCart)
+                ? cart.map((item:Item) => {
+                    if(item.inCart===true){
+                        return (
+                        <>
+                        <img src={item.image} height={'70px'} width={'70px'} alt={item.title} />
+                        <p>{item.title}</p>
+                        <p>{item.quantity} x ${item.price}</p>
+                        <p>${item.cost}</p>
+                        </>
+                        )
+                    }
+                    return null;
+                }):<p>Cart is empty!</p>}
+                {/* {cart ? 
+                    <>
+                        <img src={cart[0].image} height={'70px'} width={'70px'} alt={cart[0].title} />
+                        <p>{cart[0].title}</p>
+                        <p>{cart[0].quantity} x ${cart[0].price}</p>
+                        <p>${cart[0].cost}</p>
+                    </>
+                 : 
+                    <p>Cart is empty. Shop now!</p>
+                } */}
             </div>
             <div className="CartRight">
-            {/* item list names quantity x price  total at bottom*/}
+                {/* item list names quantity x price  total at bottom*/}
             </div>
         </div>
     )
+    
 }
 
 export default Cart;
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -18,9 +18,7 @@ interface Props {
     setData: React.Dispatch<React.SetStateAction<Item[]>>;
 }
 const Shop: React.FC<Props> = ({data,setData}) => {
-    return (
-        <h1>Shop</h1>
-    )
+    
 }
 
 export default Shop;
