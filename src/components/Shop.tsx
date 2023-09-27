bc349b129a040cdedad4bed37a46f9476ac900f1
Hardeep Singh <hs0774@gmail.com>
2023-09-27 00:01:23 -0400

    shop page works now,though ill look into it more tomorrow since i copied all the code from home to it, looks good though so styling is likely next for tomorrow

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,5 +1,8 @@
 import React from "react"; 
 import '../css/Shop.css'
+import { Link } from "react-router-dom";
+
+
 
 interface Item {
     image: string,
@@ -13,15 +16,157 @@ interface Item {
         rate:number
     },
 }
+
 interface Props {
     data: Item[];
     setData: React.Dispatch<React.SetStateAction<Item[]>>;
     orderData: Set<number>
     setOrderData: React.Dispatch<React.SetStateAction<Set<number>>>
 }
+
 const Shop: React.FC<Props> = ({data,setData,orderData,setOrderData}) => {
+    const [items, setItems] = React.useState<Item[]>([]);
+    const [orderState,setOrderState] = React.useState(orderData);
+
+    React.useEffect(() => {
+        if(data && data.length > 0){
+            setItems(data)
+        } else {
+      fetch('https://fakestoreapi.com/products?limit=15')
+        .then(res => res.json())
+        .then((json) => {
+           const createdItems =  json.map((item:Item) => ({
+                image: item.image,
+                id: item.id,
+                price: Math.floor(item.price),
+                title: item.title,
+                inCart:false,
+                quantity:1,
+                cost:Math.floor(item.price),
+                rating: {
+                    rate:item.rating.rate
+                }
+            }));
+            setItems(createdItems);
+            setData(createdItems);
+        });
+        }
+    }, [data]);
+            // console.log(data)
+
+    function handleChange(e:any, id:number){
+       // console.log(e.target.value);
+       
+        const {name, value} = e.target;
+             setItems(prevItems => 
+                prevItems.map(item => {
+                    if (id === item.id) {
+                        return {
+                            ...item,
+                            [name]: parseInt(value,10),
+                            cost: value * item.price,
+                        }
+                    }
+                    return item;
+                })
+            );
+            // setData(prevData =>
+            //     prevData.map(item => {
+            //       if (id === item.id) {
+            //        // const quantity = parseInt(value, 10);
+            //         return {
+            //           ...item,
+            //           [name]: parseInt(value,10),
+            //           cost: value * item.price,
+            //         };
+            //       }
+            //       return item;
+            //     })
+            //   );
+            // setData(items);
+            // console.log('data',data);
+            console.log('items',items);
+    }
+    function handleSubmit(e:any,id:number){
+        e.preventDefault();
+
+        console.log(e.target[0].value);
+        const {name, value} = e.target[0];
+        console.log(value,name);
+
+        parseInt(value,10) > 0 
+        ? setOrderState(prev => new Set(prev).add(id))
+        : setOrderState(prev => {
+            const newState = new Set(prev);
+            newState.delete(id);
+            return newState;
+        });
+
+        parseInt(value,10) >0
+        ? setOrderData(prev => new Set(prev).add(id))
+        : setOrderData(prev => {
+            const newState = new Set(prev);
+            newState.delete(id);
+            return newState;
+        });
+
+        setItems(prevItems => 
+            prevItems.map(item => {
+                if (id === item.id) {
+                    return {
+                        ...item,
+                        [name]:parseInt(value,10),
+                        cost:value*item.price,
+                        inCart: parseInt(value,10)>0,
+
+                    }
+                }
+                return item;
+            })  
+        );
+
+        setData(prevData => 
+            prevData.map(item => {
+                if (id === item.id) {
+                    return {
+                        ...item,
+                        [name]:parseInt(value,10),
+                        cost:value*item.price,
+                        inCart:parseInt(value,10)>0,
+                        
+                    }
+                }
+                return item;
+            })  
+        );
+        
+        // console.log('data',data);  .slice(0,3)
+        // console.log('items',items);
+    }
+
+    
     return (
-        <h1>Shop</h1>
+        <>
+        <Link to='/Shop'><button>Shop Now</button></Link>
+        <div className="HomeItems">
+            {items.map((item, index) => (
+                <div key={item.id}>
+                    <img className="HomeItemImg" src={item.image}/>
+                    <p>Title: {item.title}</p>
+                    <p>Price: {item.price}.00</p>
+                    <p>Rating: {item.rating.rate}</p>
+                    <form onSubmit  ={(e) => handleSubmit(e,item.id)}>
+                        <p>Quantity: 
+                            <input value={item.quantity} name='quantity' min='0' step='1'
+                            type="number" onChange={(e) => handleChange(e,item.id)}></input>
+                        </p>
+                        <button>Add to Cart</button>
+                    </form>  
+                    <p>$ {item.cost ? item.cost : item.price}.00</p>
+                </div>
+            ))}
+        </div>
+        </>    
     )
 }
 
